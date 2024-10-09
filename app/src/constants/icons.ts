02a78f7804e57b742f2d79d8892c0b5bb746d182
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2024-10-10 00:20:55 +0200

    add assets

---
+++ app/src/constants/icons.ts
@@ -0,0 +1,21 @@
+import google from '../assets/google.png';
+import logo from '../assets/logo.png';
+import calendarActive from '../assets/calendar-active.png';
+import calendar from '../assets/calendar.png';
+import dashboard from '../assets/dashboards.png';
+import dashboardActive from '../assets/dashboards-active.png';
+import settings from '../assets/settings.png';
+import settingsActive from '../assets/settings-active.png';
+import logout from '../assets/logout.png';
+
+export default {
+  google,
+  logo,
+  calendar,
+  calendarActive,
+  dashboard,
+  dashboardActive,
+  settings,
+  settingsActive,
+  logout,
+};
